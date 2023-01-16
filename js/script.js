827c544bfd95d7498435c2f351ee607258f40797
Vlasenko Aleksandr <baralexx181972@gmail.com>
2023-01-16 21:01:04 +0200

    learn js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const majicBtn = document.querySelector('.js-majic-btn');
+majicBtn.addEventListener('click', () => {
+  const navEl = document.querySelectorAll('li');
+  console.log(navEl);
+  console.log('navEl', navEl.length);
+});
